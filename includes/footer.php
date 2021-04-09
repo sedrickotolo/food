58c4a95c6cdf2fab786d63102572330d8825d866
sedrickotolo <sedricksedu2@gmail.com>
2021-04-09 14:26:31 +0300

    Initial

---
+++ includes/footer.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PCC</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/bistro-icons.css">
+<link rel="stylesheet" type="text/css" href="css/animate.min.css">
+<link rel="stylesheet" type="text/css" href="css/settings.css">
+<link rel="stylesheet" type="text/css" href="css/navigation.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="css/owl.transitions.css">
+<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css">
+<link rel="stylesheet" type="text/css" href="css/zerogrid.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/loader.css">
+<link rel="shortcut icon" href="images/favicon.png">
+
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+
+<!--Footer-->
+<footer class="padding-top bg_black">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-sm-6 footer_column">
+        <h4 class="heading">Who are we?</h4>
+        <hr class="half_space">
+        <p class="half_space"></p>
+        <p>Launched in Mumbai, JKS has grown from a home project to one of the largest food aggregators in the world. We are present in 24 countries and 10000+ cities globally, enabling our vision of better food for more people. We not only connect people to food in every context but work closely with restaurants to enable a sustainable ecosystem.</p>
+      </div>
+      <div class="col-md-3 col-sm-6 footer_column">
+        <h4 class="heading">Quick Links</h4>
+        <hr class="half_space">
+        <ul class="widget_links">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="food.html">Our Food</a></li>
+          <li><a href="about.html">About Us</a></li>
+
+          <li><a href="faq.html">Faq's</a></li>
+          <li><a href="./account/register.php">Order Now</a></li>
+
+        </ul>
+      </div>
+      <div class="col-md-3 col-sm-6 footer_column">
+        <h4 class="heading">Newsletter</h4>
+        <hr class="half_space">
+        <p class="icon"><i class="icon-dollar"></i>Sign up with your name and email to get updates fresh updates.</p>
+        <div id="result1" class="text-center"></div>
+
+       <form action="http://themesindustry.us13.list-manage.com/subscribe/post-json?u=4d80221ea53f3a4487ddebd93&id=494727d648&c=?" method="get" onSubmit="return false" class="newsletter">
+          <div class="form-group">
+            <input type="email" placeholder="E-mail Address" required name="EMAIL" id="EMAIL" class="form-control" />
+          </div>
+          <div class="form-group">
+            <input type="submit" class="btn-submit button3" value="Subscribe" />
+          </div>
+        </form>
+      </div>
+      <div class="col-md-3 col-sm-6 footer_column">
+        <h4 class="heading">Get in Touch</h4>
+        <hr class="half_space">
+        <p></p>
+        <p class="address"><i class="icon-location"></i>JKS, Opposite S.L.Raheja Hospital, Mahim Causeway, Mahim (West), Mumbai, Maharashtra 400016.</p>
+        <p class="address"><i class="fa fa-phone"></i>(+91) 892 808 5056</p>
+        <p class="address"><i class="icon-dollar"></i><a href="office@jks.com">office@jks.com</a></p>
+      </div>
+    </div>
+    <div class="row">
+     <div class="col-md-12">
+        <div class="copyright clearfix">
+          <p>Copyright &copy; 2020 JKS. All Right Reserved</p>
+          <ul class="social_icon">
+               <li><a href="#" class="facebook"><i class="icon-facebook5"></i></a></li>
+               <li><a href="#" class="twitter"><i class="icon-twitter4"></i></a></li>
+               <li><a href="#" class="google"><i class="icon-google"></i></a></li>
+              </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<a href="#" id="back-top"><i class="fa fa-angle-up fa-2x"></i></a>
+
+<script src="js/jquery-2.2.3.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/jquery.themepunch.tools.min.js"></script>
+<script src="js/jquery.themepunch.revolution.min.js"></script>
+<script src="js/revolution.extension.layeranimation.min.js"></script>
+<script src="js/revolution.extension.navigation.min.js"></script>
+<script src="js/revolution.extension.parallax.min.js"></script>
+<script src="js/revolution.extension.slideanims.min.js"></script>
+<script src="js/revolution.extension.video.min.js"></script>
+<script src="js/slider.js" type="text/javascript"></script>
+<script src="js/owl.carousel.min.js" type="text/javascript"></script>
+<script src="js/jquery.parallax-1.1.3.js"></script>
+<script src="js/jquery.mixitup.min.js"></script>
+<script src="js/jquery-countTo.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/functions.js" type="text/javascript"></script>
