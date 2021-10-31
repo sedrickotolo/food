7a03e4097eb5159eeb06518416033e4bdeae81a6
sedrickotolo <sedricksedu2@gmail.com>
2021-10-31 16:53:03 +0300

    logo

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -3,8 +3,17 @@ session_start();
 $servername = "localhost";
 $server_user = "root";
 $server_pass = "";
-$dbname = "onlinefood";
+$dbname = "food";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
+
+// Check connection
+if (!$con) {       //checking connection to DB	
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
+
+
+
+
